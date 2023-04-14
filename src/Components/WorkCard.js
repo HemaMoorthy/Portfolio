12728e74248ad80a3eaa2bb094b6a651139d379c
Hema Moorthy <hemamoorthy8@gmail.com>
2023-04-14 20:20:07 +0530

    rh

--- src/Components/WorkCard.js
+++ src/Components/WorkCard.js
@@ -4,13 +4,10 @@ import proj1 from "../assets/web.jpg";
 import { NavLink } from 'react-router-dom';
 const WorkCard = () => {
   return (
-    <div className='work-container'>
-        <h1 className='project-heading'>Projects</h1>
-        <div className='project-container'>
-            <div className='project-card'>
-                <img src={proj1} alt='image'/>
-                <h2 className='project-title'>Project Title</h2>
-                <div className='pro-details'>
+    <div className='project-card'>
+        <img src={proj1} alt='image'/>
+        <h2 className='project-title'>Project Title</h2>
+            <div className='pro-details'>
                 <p>This is details of the project</p>
                     <div className='pro-btns'>
                         <NavLink to="url.com" className="btn">View</NavLink>
@@ -18,8 +15,6 @@ const WorkCard = () => {
                     </div>
                 </div>
             </div>
-        </div>
-    </div>
   )
 }
 
--- src/Components/WorkCardData.js
+++ src/Components/WorkCardData.js
@@ -7,6 +7,6 @@ const projectCardData = [
     {
         imgsrc:pro1,
         title:"Ecommerce Website",
-        text:"lorem "
+        text:"Lorem ipsum dolor sit amet. Ut quis ducimus et assumenda quia At nulla dolor ea eligendi dolorum sed neque expedita qui cumque excepturi. 33 nulla natus ea minus omnis rem odio recusandae. Ex assumenda esse et quidem voluptatum est alias maxime. "
     }
 ]
