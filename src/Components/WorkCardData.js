d722e78ca0e6a5cd4c9e89450472a658b324f338
Hema Moorthy <hemamoorthy8@gmail.com>
2023-04-14 20:19:32 +0530

    data

---
+++ src/Components/WorkCardData.js
@@ -0,0 +1,12 @@
+
+import proj1 from "../assets/web.jpg";
+import proj2 from "../assets/home.jpg";
+import proj3 from "../assets/programing.jpg";
+
+const projectCardData = [
+    {
+        imgsrc:pro1,
+        title:"Ecommerce Website",
+        text:"lorem "
+    }
+]
